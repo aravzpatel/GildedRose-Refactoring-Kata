2135cbac37ae420c7dbaf32aebae94eae22521c1
Arav <arav.patel@gmail.com>
2020-08-12 10:51:29 +0100

    Create backstage pass testing framework

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -60,7 +60,43 @@ describe("Gilded Rose", function() {
       expect(items[0].sellIn).toEqual(-2);
       expect(items[0].quality).toEqual(50);
     })
+  })
 
+  describe("backstage pass", function(){
+    it("11 days before the sellin, it should increase in quality by 1", function(){
+      const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 11, 3) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(10);
+      expect(items[0].quality).toEqual(4);
+    })
+
+    it("6 days before the sellin, it should increase in quality by 2", function(){
+      const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 7, 3) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(6);
+      expect(items[0].quality).toEqual(5);
+    })
+
+    it("3 days before the sellin, it should increase in quality by 2", function(){
+      const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 3, 3) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(2);
+      expect(items[0].quality).toEqual(6);
+    })
+
+    it("it should never have a quality greater than 50", function(){
+      const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 1, 50) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(0);
+      expect(items[0].quality).toEqual(50);
+    })
+
+    it("after the sellin, it should have a quality of 0", function(){
+      const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 3) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(-1);
+      expect(items[0].quality).toEqual(0);
+    })
   })
 
 });
